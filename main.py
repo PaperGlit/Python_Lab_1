0f9f160f5370fb0fbb0575afe3d04e09a3d414a4
Glit <vpad.lazar.04@gmail.com>
2024-09-07 21:19:57 +0300

    General refactoring / optimizations before history implementation

--- main.py
+++ main.py
@@ -1,88 +1,78 @@
 def calculator():
-    operands = ["+", "-", "*", "/", "^", "root", "%"]
     memory = 0.0
     result = 0.0
+    memory_operations = ["ms", "m+", "m-"]
     while True:
+        num1 = validate_num(memory, "Enter first number (or MR / MC): ")
 
-        while True:
-            value1 = (input("Enter first number (or MR / MC): "))
-            if value1.lower() == "mr":
-                num1 = memory
-                print("Recovered value: " + str(num1))
-                break
-            elif value1.lower() == "mc":
-                memory = 0
-                print("Memory cleared!")
-                continue
-            else:
-                 try:
-                     num1 = float(value1)
-                     break
-                 except ValueError:
-                    print("Please enter a number")
-                    continue
-
-        operator_is_memory = False
-        while True:
-            operator = input("Enter operator: ")
-            if operator.lower() == "ms":
-                # noinspection PyUnboundLocalVariable
-                memory = num1
-                print("The value was stored in the memory!")
-                operator_is_memory = True
-                break
-            elif operator not in operands:
-                print("Please enter a valid operator")
-                continue
-            break
-        if operator_is_memory:
+        operator = validate_operator()
+        if operator == "ms":
+            memory = num1
+            print("Memory value stored! Current value: " + str(memory))
             continue
 
-        while True:
-            value2 = (input("Enter second number (or MR / MC): "))
-            if value2.lower() == "mr":
-                num2 = memory
-                print("Recovered value: " + str(num2))
-                break
-            elif value2.lower() == "mc":
-                memory = 0
-                print("Memory cleared!")
-                continue
-            else:
-                try:
-                    num2 = float(value2)
-                    break
-                except ValueError:
-                    print("Please enter a number")
-                    continue
+        num2 = validate_num(memory, "Enter second number (or MR / MC): ")
 
-        # noinspection PyUnboundLocalVariable
         if operator == "/" and num2 == 0:
             print("Error: cannot divide by zero")
         else:
             result = calculate(num1, num2, operator)
-            # noinspection PyUnboundLocalVariable
             print("Result : " + str(result))
 
         try_again = input("Would you like to try again? (Y / N) // Store memory? (MS / M+ / M-): ").lower()
+        if try_again in memory_operations:
+            memory = validate_memory(try_again, memory, result)
+        elif try_again == "y":
+            result = 0.0
+            continue
+        else:
+            break
 
-        match try_again:
-            case "y":
-                continue
-            case "ms":
-                memory = result
-                print("Memory stored! Current value: " + str(memory))
-                continue
-            case "m+":
-                memory += result
-                print("Memory updated! Current value: " + str(memory))
-                continue
-            case "m-":
-                memory -= result
-                print("Memory updated! Current value: " + str(memory))
-                continue
-            case _:
-                break
+
+def validate_num(memory, prompt):
+    while True:
+        value = (input(prompt)).lower()
+        if value.lower() == "mr":
+            print("Recovered value: " + str(memory))
+            return memory
+        elif value.lower() == "mc":
+            memory = 0.0
+            print("Memory cleared!")
+        else:
+            try:
+                return float(value)
+            except ValueError:
+                print("Please enter a valid number / memory operation")
+
+
+def validate_operator():
+    operands = ["+", "-", "*", "/", "^", "root", "%"]
+    while True:
+        operator = (input("Enter operator (or MS): ")).lower()
+        if operator == "ms":
+            return operator
+        elif operator in operands:
+            return operator
+        else:
+            print("Please enter a valid operator")
+
+
+def validate_memory(value, memory, num):
+    match value:
+        case "ms":
+            memory = num
+            print("Memory value stored! Current value: " + str(memory))
+        case "m+":
+            memory += num
+            print("Memory value updated! Current value: " + str(memory))
+        case "m-":
+            memory -= num
+            print("Memory value updated! Current value: " + str(memory))
+        case _:
+            print("Error occurred during memory validation")
+            return 0.0
+    return memory
+
 
 def calculate(num1, num2, operator):
     match operator:
