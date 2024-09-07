af208f54620199b9d7cf9079718792a6161211b8
Glit <vpad.lazar.04@gmail.com>
2024-09-07 20:28:51 +0300

    Calculator memory implementation

--- main.py
+++ main.py
@@ -1,43 +1,88 @@
 def calculator():
     operands = ["+", "-", "*", "/", "^", "root", "%"]
+    memory = 0.0
+    result = 0.0
     while True:
 
         while True:
-            try:
-                num1 = float(input("Enter first number: "))
+            value1 = (input("Enter first number (or MR / MC): "))
+            if value1.lower() == "mr":
+                num1 = memory
+                print("Recovered value: " + str(num1))
                 break
-            except ValueError:
-                print("Please enter a number")
+            elif value1.lower() == "mc":
+                memory = 0
+                print("Memory cleared!")
                 continue
+            else:
+                 try:
+                     num1 = float(value1)
+                     break
+                 except ValueError:
+                    print("Please enter a number")
+                    continue
 
+        operator_is_memory = False
         while True:
             operator = input("Enter operator: ")
-            if operator not in operands:
+            if operator.lower() == "ms":
+                # noinspection PyUnboundLocalVariable
+                memory = num1
+                print("The value was stored in the memory!")
+                operator_is_memory = True
+                break
+            elif operator not in operands:
                 print("Please enter a valid operator")
                 continue
             break
+        if operator_is_memory:
+            continue
 
         while True:
-            try:
-                num2 = float(input("Enter second number: "))
+            value2 = (input("Enter second number (or MR / MC): "))
+            if value2.lower() == "mr":
+                num2 = memory
+                print("Recovered value: " + str(num2))
                 break
-            except ValueError:
-                print("Please enter a number")
+            elif value2.lower() == "mc":
+                memory = 0
+                print("Memory cleared!")
                 continue
+            else:
+                try:
+                    num2 = float(value2)
+                    break
+                except ValueError:
+                    print("Please enter a number")
+                    continue
 
         # noinspection PyUnboundLocalVariable
         if operator == "/" and num2 == 0:
-            return "Error: cannot divide by zero"
+            print("Error: cannot divide by zero")
+        else:
+            result = calculate(num1, num2, operator)
+            # noinspection PyUnboundLocalVariable
+            print("Result : " + str(result))
 
-        # noinspection PyUnboundLocalVariable
-        print("Result : " + str(calculate(num1, num2, operator)))
+        try_again = input("Would you like to try again? (Y / N) // Store memory? (MS / M+ / M-): ").lower()
 
-        try_again = input("Would you like to try again? (y/n): ").lower()
-
-        if try_again != "y":
-            break
-
-
+        match try_again:
+            case "y":
+                continue
+            case "ms":
+                memory = result
+                print("Memory stored! Current value: " + str(memory))
+                continue
+            case "m+":
+                memory += result
+                print("Memory updated! Current value: " + str(memory))
+                continue
+            case "m-":
+                memory -= result
+                print("Memory updated! Current value: " + str(memory))
+                continue
+            case _:
+                break
 
 def calculate(num1, num2, operator):
     match operator:
@@ -57,5 +102,6 @@ def calculate(num1, num2, operator):
             return num1 % num2
         case _:
             return "Error occurred during calculation"
+
 
 calculator()
