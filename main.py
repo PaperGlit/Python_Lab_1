f87bf6f8b276e110886a68e1f082f85ebedc7ad9
Vladyslav Lazar <vladyslav.lazar.ri.2022@lpnu.ua>
2024-09-07 12:25:14 +0300

    rewrite of the calculation pipeline

--- main.py
+++ main.py
@@ -1,37 +1,61 @@
-def parse(string):
-    #Out of those 3 variables, only num must be declared (the other 2 are just a safeguard)
-    num = ""
-    num1 = ""
-    operator = ""
-    for char in string:
-        if char.isnumeric() or char == ".":
-            num += char
-            continue
-        elif char == '+' or char == '-' or char == '*' or char == '/':
-            operator = char
-            num1 = float(num)
-            num = ""
-            continue
-        else:
-            print("Unknown character detected!")
+def calculator():
+    operands = ["+", "-", "*", "/", "^", "root", "%"]
+    while True:
+
+        while True:
+            try:
+                num1 = float(input("Enter first number: "))
+                break
+            except ValueError:
+                print("Please enter a number")
+                continue
+
+        while True:
+            operator = input("Enter operator: ")
+            if operator not in operands:
+                print("Please enter a valid operator")
+                continue
             break
-    num2 = float(num)
-    return calculate(num1, num2, operator)
+
+        while True:
+            try:
+                num2 = float(input("Enter second number: "))
+                break
+            except ValueError:
+                print("Please enter a number")
+                continue
+
+        # noinspection PyUnboundLocalVariable
+        if operator == "/" and num2 == 0:
+            return "Error: cannot divide by zero"
+
+        # noinspection PyUnboundLocalVariable
+        print("Result : " + str(calculate(num1, num2, operator)))
+
+        try_again = input("Would you like to try again? (y/n): ").lower()
+
+        if try_again != "y":
+            break
+
 
 
 def calculate(num1, num2, operator):
     match operator:
-        case '+':
+        case "+":
             return num1 + num2
-        case '-':
+        case "-":
             return num1 - num2
-        case '*':
+        case "*":
             return num1 * num2
-        case '/':
+        case "/":
             return num1 / num2
+        case "^":
+            return num1 ** num2
+        case "root":
+            return num1 ** (1 / num2)
+        case "%":
+            return num1 % num2
         case _:
             return "Error occurred during calculation"
 
-
-inputString = input("Enter a string: ")
-print(parse(inputString))
+calculator()
